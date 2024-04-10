fde0eff16dc589b28aae3084cc2a9ac1246d33e2
Janthony <88044282+Joshuaberck@users.noreply.github.com>
2024-04-10 13:33:29 -0500

    Update PlayerMovement.cs

--- ExileProjecttt/Assets/Cainos/Pixel Art Top Down - Basic/Script/PlayerMovement.cs
+++ ExileProjecttt/Assets/Cainos/Pixel Art Top Down - Basic/Script/PlayerMovement.cs
@@ -15,7 +15,6 @@ public class PlayerMovement : MonoBehaviour
 
     public float interactDistance = 2f; // The distance at which the player can interact with the tribe leader
     private GameObject tribeLeader; // Reference to the tribe leader GameObject
-    private TribeLeaderDialogue tribeLeaderDialogue;
 
     public AudioSource dash;
 
@@ -24,8 +23,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         // Find the GameObject with the name "Tribe Leader" in the scene
-        tribeLeader = GameObject.Find("Tribe Leader");
-        tribeLeaderDialogue = tribeLeader.GetComponent<TribeLeaderDialogue>();
+        
 
     }
 
@@ -44,10 +42,7 @@ public class PlayerMovement : MonoBehaviour
         FlipSprite();
 
         // Check for interaction with the tribe leader
-        if (Input.GetButtonDown("Interact"))
-        {
-            InteractWithTribeLeader();
-        }
+        
     }
 
     private void FixedUpdate()
@@ -88,13 +83,5 @@ public class PlayerMovement : MonoBehaviour
         isDashing = false;
     }
 
-    void InteractWithTribeLeader()
-    {
-        // Check if the tribe leader is within the interaction distance
-        if (tribeLeader != null && Vector2.Distance(transform.position, tribeLeader.transform.position) <= interactDistance)
-        {
-            // Interact with the tribe leader
-            tribeLeaderDialogue.DisplayNextLine();
-        }
-    }
+    
 }
