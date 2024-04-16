e4a86a7d49711663ecf6be39f342ca8c6757f919
NotShuang <shuangxie06@gmail.com>
2024-04-16 13:46:32 -0500

    ...

---
+++ ExileProjecttt/Assets/NPC.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
